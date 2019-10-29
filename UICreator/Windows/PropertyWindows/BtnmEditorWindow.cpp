45579dd887f89a3d17249c611bca46ecbabfc54a
Ron Ohmer <rohmer@yahoo.com>
2019-10-28 22:24:13 -0400

    Working on editor

--- UICreator/Windows/PropertyWindows/BtnmEditorWindow.cpp
+++ UICreator/Windows/PropertyWindows/BtnmEditorWindow.cpp
@@ -1,15 +1,19 @@
 #include "BtnmEditorWindow.h"
-lv_obj_t* BtnmEditorWindow::window, * BtnmEditorWindow::preview, * BtnmEditorWindow::previewCont;
+lv_obj_t* BtnmEditorWindow::window, * BtnmEditorWindow::preview, * BtnmEditorWindow::previewCont, * BtnmEditorWindow::recolor,
+* BtnmEditorWindow::oneToggle, * BtnmEditorWindow::allCont, * BtnmEditorWindow::width, * BtnmEditorWindow::height,
+*BtnmEditorWindow::buttonWindow;
+lv_obj_t *BtnmEditorWindow::addLeft, * BtnmEditorWindow::addRight, * BtnmEditorWindow::addAbove, * BtnmEditorWindow::addBelow,
+    * BtnmEditorWindow::btnLabel, *BtnmEditorWindow::delBtn, *BtnmEditorWindow::btnWidth;
 int BtnmEditorWindow::listContHeight, BtnmEditorWindow::listContWidth;
 PropertyWindow* BtnmEditorWindow::pw;
 std::vector<BtnmEditorWindow::btnStruct> BtnmEditorWindow::buttons;
+bool BtnmEditorWindow::buttonEdDrawn = false;
 
 void BtnmEditorWindow::resize()
 {
     int btnmW, btnmH;
-    lv_obj_t* btnm = pw->GetSelectedObject();
-    btnmW = lv_obj_get_width(btnm);
-    btnmH = lv_obj_get_height(btnm);
+    btnmW = lv_obj_get_width(preview);
+    btnmH = lv_obj_get_height(preview);
 
     int windowWidth;
     if (btnmW < 50)
@@ -26,9 +30,20 @@ void BtnmEditorWindow::resize()
     int posY = lv_obj_get_height(lv_scr_act()) / 2 - windowHeight / 2;
     lv_obj_set_pos(window, posX, posY);
     lv_obj_set_size(previewCont, btnmW + 10, btnmH + 30);
-    listContWidth = windowWidth - lv_obj_get_width(previewCont) - 20;
-    listContHeight = windowHeight / 2;
 
+    lv_obj_set_pos(allCont, btnmW + 15, 5);
+    lv_obj_set_size(allCont, windowWidth-btnmW-20, 90);
+    lv_obj_set_pos(oneToggle, windowWidth - btnmW - 150, 20);
+    if ( lv_obj_get_y(previewCont)<90)
+    {
+        lv_obj_set_pos(buttonWindow, 5, lv_obj_get_y(allCont)+lv_obj_get_height(allCont)+30);
+    }
+    else
+    {
+        int pY = lv_obj_get_height(previewCont);
+        lv_obj_set_pos(buttonWindow, 5, lv_obj_get_height(previewCont) + lv_obj_get_y(previewCont)+15);
+    }
+    lv_obj_set_size(buttonWindow, windowWidth - 10, windowHeight - btnmH -100);
 }
 
 void BtnmEditorWindow::CreateBtnmEditorWindow(PropertyWindow* propertyWindow)
@@ -48,6 +63,61 @@ void BtnmEditorWindow::CreateBtnmEditorWindow(PropertyWindow* propertyWindow)
     lv_obj_set_pos(preview, 5, 25);
     lv_obj_t* l = lv_label_create(previewCont, nullptr);
     lv_label_set_text(l, "Preview");
+
+    allCont = lv_cont_create(window, nullptr);
+    recolor=lv_cb_create(allCont, nullptr);
+    lv_obj_t* allContL = lv_label_create(allCont, nullptr);
+    lv_label_set_text(allContL, "Button Map Settings");
+    lv_obj_set_pos(allContL, 5, 5);
+
+    lv_cb_set_text(recolor, "Recolor");
+    lv_cb_set_checked(recolor,lv_btnm_get_recolor(preview));
+    lv_obj_set_pos(recolor, 5, 20);
+
+    oneToggle = lv_cb_create(allCont, nullptr);
+    lv_cb_set_text(oneToggle, "One Toggle");
+    lv_cb_set_checked(oneToggle, lv_btnm_get_one_toggle(preview));
+    lv_obj_set_pos(oneToggle, 75, 20);
+    lv_obj_set_event_cb(oneToggle, oneToggleCB);
+    lv_obj_set_event_cb(recolor, recolorCB);
+
+    lv_obj_t* sizeLab = lv_label_create(allCont, nullptr);
+    lv_label_set_text(sizeLab, "Size: ");
+    lv_obj_set_pos(sizeLab, 5, 60);
+
+    lv_obj_t* xLab = lv_label_create(allCont, nullptr);
+    lv_obj_t* yLab = lv_label_create(allCont, nullptr);
+    lv_label_set_text(xLab, "X:");
+    lv_label_set_text(yLab, "Y:");
+    lv_obj_set_pos(xLab, 50, 60);
+    lv_obj_set_pos(yLab, 130, 60);
+
+    width = lv_ta_create(allCont, nullptr);
+    lv_ta_set_accepted_chars(width, "0123456789");
+    height = lv_ta_create(allCont, nullptr);
+    lv_ta_set_accepted_chars(height, "0123456789");
+    lv_obj_set_size(width, 50, 30);
+    lv_obj_set_size(height, 50, 30);
+    lv_obj_set_pos(width, 65, 50);
+    lv_obj_set_pos(height, 145, 50);
+    
+    std::stringstream ss;
+    ss << lv_obj_get_width(preview);
+    lv_ta_set_text(width, ss.str().c_str());
+    ss.str("");
+    ss << lv_obj_get_height(preview);
+    lv_ta_set_text(height, ss.str().c_str());
+    lv_ta_set_cursor_type(width, LV_CURSOR_NONE);
+    lv_ta_set_cursor_type(height, LV_CURSOR_NONE);
+    lv_obj_set_user_data(width, 0);
+    lv_obj_set_user_data(height, (lv_obj_user_data_t)1);
+
+    buttonWindow = lv_cont_create(window, nullptr);
+    lv_obj_t* bwLab = lv_label_create(buttonWindow, nullptr);
+    lv_label_set_text(bwLab, "Button Properties");
+
+    lv_obj_set_event_cb(width, taCB);
+    lv_obj_set_event_cb(height, taCB);
     loadButtons();
     resize();
 }
@@ -105,6 +175,73 @@ void BtnmEditorWindow::loadButtons()
     
 }
 
+void BtnmEditorWindow::createButtonEd()
+{
+    addLeft = lv_btn_create(buttonWindow, nullptr);
+    lv_obj_t* l1 = lv_label_create(addLeft, nullptr);
+    std::stringstream ss;
+    ss << LV_SYMBOL_LEFT << "Add Btn to Left";
+    lv_label_set_text(l1, ss.str().c_str());
+    ss.str("");
+    lv_obj_set_size(addLeft, 100, 30);
+    int mid = (lv_obj_get_height(buttonWindow)-60) / 2;
+    lv_obj_set_pos(addLeft, 5, mid);
+
+    addAbove = lv_btn_create(buttonWindow, nullptr);
+    lv_obj_t* l2 = lv_label_create(addAbove, nullptr);
+    ss << LV_SYMBOL_UP << "Add Btn Above";
+    lv_label_set_text(l2, ss.str().c_str());
+    ss.str("");
+    lv_obj_set_size(addAbove, 100, 30);
+    lv_obj_set_pos(addAbove, 140, 15);
+
+    btnLabel = lv_ta_create(buttonWindow, nullptr);
+    lv_obj_set_size(btnLabel, 100, 30);
+    lv_obj_set_pos(btnLabel, 120, mid);
+    lv_obj_set_user_data(btnLabel, (lv_obj_user_data_t)2);
+    lv_obj_set_event_cb(btnLabel, taCB);
+
+    delBtn = lv_btn_create(buttonWindow, nullptr);
+    lv_obj_t* l3 = lv_label_create(delBtn, nullptr);
+    lv_label_set_text(l3, LV_SYMBOL_TRASH);
+    lv_obj_set_size(delBtn, 30, 30);
+    lv_obj_set_pos(delBtn, 225, mid);
+
+    addRight = lv_btn_create(buttonWindow, nullptr);
+    lv_obj_t* l4 = lv_label_create(addRight, nullptr);
+    ss << LV_SYMBOL_RIGHT << "Add Btn to Right";
+    lv_label_set_text(l4, ss.str().c_str());
+    ss.str("");
+    lv_obj_set_size(addRight, 100, 30);
+    lv_obj_set_pos(addRight, 260, mid);
+
+    addBelow = lv_btn_create(buttonWindow, nullptr);
+    lv_obj_t* l5 = lv_label_create(addBelow, nullptr);
+    ss << LV_SYMBOL_DOWN << "Add Btn Below";
+    lv_label_set_text(l5, ss.str().c_str());
+    ss.str("");
+    lv_obj_set_size(addBelow, 100, 30);
+    lv_obj_set_pos(addBelow, 140, mid+40);
+
+    lv_obj_t* l6 = lv_label_create(buttonWindow, nullptr);
+    lv_label_set_text(l6, "Button Width:");
+    lv_obj_set_pos(l6, lv_obj_get_width(buttonWindow) - 140,20);
+
+    btnWidth = lv_ta_create(buttonWindow, nullptr);
+    lv_ta_set_cursor_type(btnWidth, LV_CURSOR_NONE);
+    lv_obj_set_size(btnWidth, 30, 25);
+    lv_obj_set_pos(btnWidth, lv_obj_get_width(buttonWindow) - 50, 10);
+
+
+}
+
+void BtnmEditorWindow::updateButtonEd(int buttonNum)
+{
+    if (!buttonEdDrawn)
+        createButtonEd();
+    buttonEdDrawn = true;
+}
+
 void BtnmEditorWindow::closeBtnCB(lv_obj_t* obj, lv_event_t event)
 {
     if (event == LV_EVENT_CLICKED)
@@ -113,8 +250,54 @@ void BtnmEditorWindow::closeBtnCB(lv_obj_t* obj, lv_event_t event)
 
 void BtnmEditorWindow::previewCB(lv_obj_t* obj, lv_event_t event)
 {
-    if (event != LV_EVENT_CLICKED)
+    if (event != LV_EVENT_VALUE_CHANGED)
         return;
+    int buttonNum=lv_btnm_get_active_btn(preview);
+    updateButtonEd(buttonNum);
+}
+
+void BtnmEditorWindow::recolorCB(lv_obj_t* obj, lv_event_t event)
+{
+    if (event == LV_EVENT_CLICKED)
+    {
+        lv_btnm_set_recolor(preview, lv_cb_is_checked(obj));
+    }
+}
+
+void BtnmEditorWindow::oneToggleCB(lv_obj_t* obj, lv_event_t event)
+{
+    if (event == LV_EVENT_CLICKED)
+    {
+        lv_btnm_set_one_toggle(preview, lv_cb_is_checked(obj));
+    }
+}
+
+void BtnmEditorWindow::taCB(lv_obj_t* obj, lv_event_t event)
+{
+    if (event == LV_EVENT_CLICKED)
+    {
+        lv_ta_set_cursor_type(obj, LV_CURSOR_LINE);
+        lv_group_t* kbGroup = pw->GetKBGroup();
+        lv_group_remove_all_objs(kbGroup);
+        lv_group_add_obj(kbGroup, obj);
+        return;
+    }
+    if (event == LV_EVENT_VALUE_CHANGED)
+    {
+        std::string val = lv_ta_get_text(obj);
+        if (val.empty())
+            return;
+        int value = atoi(val.c_str());
+        if (value == 0)
+            return;
+        int i = (int)lv_obj_get_user_data(obj);
+        if (i == 0)
+            lv_obj_set_width(preview, value);
+        if (i == 1)
+            lv_obj_set_height(preview, value);
+
+        resize();
+    }
 }
 
 uint8_t BtnmEditorWindow::get_button_width(lv_btnm_ctrl_t ctrl_bits)
--- UICreator/Windows/PropertyWindows/BtnmEditorWindow.h
+++ UICreator/Windows/PropertyWindows/BtnmEditorWindow.h
@@ -20,13 +20,19 @@ public:
     };
 
 private:
-    static lv_obj_t* window, * preview, *previewCont;    
+    static lv_obj_t* window, * preview, * previewCont, * recolor, * oneToggle, * allCont,
+        * width, * height, *buttonWindow;
+    static lv_obj_t* addLeft, * addRight, * addAbove, * addBelow, * btnLabel, *delBtn, *btnWidth;
 
     static int listContWidth, listContHeight;
     static PropertyWindow* pw;
 
     static void closeBtnCB(lv_obj_t* obj, lv_event_t event);
     static void previewCB(lv_obj_t* obj, lv_event_t event);
+    static void recolorCB(lv_obj_t* obj, lv_event_t event);
+    static void oneToggleCB(lv_obj_t* obj, lv_event_t event);
+    static void taCB(lv_obj_t* obj, lv_event_t event);
+    static void btnDel(lv_obj_t* obj, lv_event_t event);
 
     static void resize();    
     static void loadButtons();
@@ -34,5 +40,9 @@ private:
     static std::vector<btnStruct> buttons;
 
     static uint8_t get_button_width(lv_btnm_ctrl_t ctrl_bits);
-    
+    static bool buttonEdDrawn;
+
+    static void updateButtonEd(int buttonNum);
+    static void createButtonEd();
+
 };
